3d1a177024a0a76ef0c260e786b572352b08d1f3
Amoong <moo1323@naver.com>
2024-04-21 12:38:26 +0900

    Add coin pickup SFX

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-
+    [SerializeField] AudioClip coinPickupSFX;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Destroy(gameObject);
+            PickupCoin();
         }
+    }
+
+    void PickupCoin()
+    {
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        Destroy(gameObject);
     }
 }
